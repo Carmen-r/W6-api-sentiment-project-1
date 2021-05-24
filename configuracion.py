e5c89ed7b42cb5bac64cb272d3d0c72d7d025576
CarmenRodríguez <carmenrgguaras@hotmail.com>
2021-05-24 23:35:57 +0200

    m

--- configuracion.py
+++ configuracion.py
@@ -3,7 +3,7 @@ import dotenv
 from pathlib import Path  # python3 only
 env_path = os.path.join(os.path.dirname(__file__), '.env')
 dotenv.load_dotenv(dotenv_path=env_path)
-
+b
 
 # Connect to the database
 dbURL = os.getenv("MONGODB_URL")
